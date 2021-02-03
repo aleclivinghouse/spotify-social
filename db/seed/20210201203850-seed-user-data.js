4f04709187302cf30609607364e81d0be2322850
aleclivinghouse <aleclivinghouse@yahoo.com>
2021-02-03 12:51:13 -0800

    comments commentlikes postcomments added

--- db/seed/20210201203850-seed-user-data.js
+++ db/seed/20210201203850-seed-user-data.js
@@ -1,4 +1,4 @@
-
+const faker = require("faker");
 const artistList = require("../seed-helpers/artist-seed-helper").artistList;
 const albumList = require("../seed-helpers/album-seed-helper").albumList;
 const trackList = require("../seed-helpers/track-seed-helper").trackList;
@@ -114,6 +114,55 @@ const createPosts = (tracks, userIds) => {
   return posts;
 }
 
+const createComments = (posts, userIds) => {
+  const comments = [];
+  for(let i = 0; i < posts.length * 4; i++){
+    const randomPostIndex =  Math.floor(Math.random() * posts.length) + 1;
+    const randomUserIndex = Math.floor(Math.random() * userIds.length) + 1;
+    let date = new Date();
+    comments.push({
+      text: faker.lorem.sentence(),
+      UserId: userIds[randomUserIndex-1].id,
+      PostId: posts[randomPostIndex-1].id,
+      createdAt: date,
+      updatedAt: date
+    })
+  }
+  return comments;
+}
+
+const createPostLikes = (posts, userIds) => {
+  const postLikes = [];
+  for(let i = 0; i < posts.length * 4; i++){
+    const randomPostIndex =  Math.floor(Math.random() * posts.length) + 1;
+    const randomUserIndex = Math.floor(Math.random() * userIds.length) + 1;
+    let date = new Date();
+    postLikes.push({
+      UserId: userIds[randomUserIndex-1].id,
+      PostId: posts[ randomPostIndex-1].id,
+      createdAt: date,
+      updatedAt: date
+    })
+  }
+  return postLikes;
+}
+
+const createCommentLikes = (comments, userIds) => {
+  const commentLikes = [];
+  for(let i = 0; i < comments.length * 2; i++){
+    const randomCommentIndex =  Math.floor(Math.random() * comments.length) + 1;
+    const randomUserIndex = Math.floor(Math.random() * userIds.length) + 1;
+    let date = new Date();
+    commentLikes.push({
+      UserId: userIds[randomUserIndex-1].id,
+      PostcommentId: comments[ randomCommentIndex-1].id,
+      createdAt: date,
+      updatedAt: date
+    })
+  }
+  return commentLikes;
+}
+
 const createUsers = () => {
   let data = [];
   for(let i = 1; i <= 50; i++){
@@ -139,17 +188,30 @@ exports.up = async ( queryInterface, Sequelize ) => {
   //add the the artists, return their id and name
   const artistDump = await createArtists();
   const artists = await queryInterface.bulkInsert({tableName: 'Artists'}, artistDump, {returning: ['id', 'name']});
-  console.log("these are the artists ", artists);
+
   const albumDump = await createAlbums(artists);
   const albums = await queryInterface.bulkInsert({tableName: 'Albums'}, albumDump, {returning: ['id', 'title', 'ArtistId', 'spotify_id']});
-  console.log("these are the albums ", albums);
+
   const trackDump = await createTracks(albums);
   const tracks = await queryInterface.bulkInsert({tableName: 'Tracks'}, trackDump, {returning: ['id', 'title', 'ArtistId', 'AlbumId', 'spotify_id']});
-  console.log("these are the tracks ", tracks);
+ 
   const postDump = await createPosts(tracks, userIds);
-  const posts = await queryInterface.bulkInsert({tableName: 'Posts'}, postDump, {returning: true});
-  console.log("these are the posts ", posts);
+  const posts = await queryInterface.bulkInsert({tableName: 'Posts'}, postDump, {returning: ['id', 'UserId']});
+ 
+  const commentDump = await createComments(posts, userIds);
+  const comments = await queryInterface.bulkInsert({tableName: 'Postcomments'}, commentDump, {returning: ['id', 'UserId', 'PostId', 'text']});
+  console.log("these are the comments:  ", comments);
+  console.log("its breaking after post comments ")
 
+  const postLikesDump = await createPostLikes(posts, userIds);
+  const postLikes = await queryInterface.bulkInsert({tableName: 'Postlikes'}, postLikesDump, {returning: ['id', 'UserId', 'PostId']});
+  console.log("these are the post likes: ", postLikes);
+  console.log("its breaking after post comments ")
+
+
+  const commentLikesDump = await createCommentLikes(comments, userIds);
+  const commentLikes = await queryInterface.bulkInsert({tableName: 'Commentlikes'}, commentLikesDump, {returning: ['id', 'UserId', 'PostcommentId']});
+  console.log("these are the comment likes ", commentLikes);
   // //add the albums, return their id and name
   // const albumIds = await queryInterface.bulkInsert({tableName: 'Albums'}, spotifyObj.albums, {returning: ['id', 'name']});
   // console.log("these are the tracks")
@@ -158,8 +220,11 @@ exports.up = async ( queryInterface, Sequelize ) => {
 };
 
 exports.down = async ( queryInterface ) => {
-   await queryInterface.bulkDelete( 'Users', null, {} );
+  await queryInterface.bulkDelete( 'Commentlikes', null, {} );
+   await queryInterface.bulkDelete( 'Postlikes', null, {} );
+   await queryInterface.bulkDelete( 'Postcomments', null, {} );
    await queryInterface.bulkDelete( 'Posts', null, {} );
+   await queryInterface.bulkDelete( 'Users', null, {} );
    await queryInterface.bulkDelete( 'Tracks', null, {} );
    await queryInterface.bulkDelete( 'Albums', null, {} );
    await queryInterface.bulkDelete( 'Artists', null, {} );
